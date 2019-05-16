359cd78e7d25bdc72e6f6d997337b240ae9d92e7
sharonpamela <sharon.pamela.santana@gmail.com>
2019-05-16 09:12:33 -0700

    Updates

---
+++ precache-manifest.755b5c266208d3fdc0106890bafa37db.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990c6428dd98a99174f539769ba76647",
+    "url": "/tagCloud/index.html"
+  },
+  {
+    "revision": "2bd98ce33d1a010fa92b",
+    "url": "/tagCloud/static/js/2.a1db4d28.chunk.js"
+  },
+  {
+    "revision": "5715c5bcf7d54018f497",
+    "url": "/tagCloud/static/js/main.68829a56.chunk.js"
+  },
+  {
+    "revision": "198e661742d57be40c8b",
+    "url": "/tagCloud/static/js/runtime~main.d55c918b.js"
+  }
+]);
